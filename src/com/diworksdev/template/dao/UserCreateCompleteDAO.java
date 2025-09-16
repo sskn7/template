048da4fd051af34ce6139cd03ca8435ad96a5668
kana sasazawa <kana.yellow7@gmail.com>
2025-09-16 22:35:50 +0900

    docs :heavy_plus_sign: #userCreateComplete.jsp,UserCreateCompleteAction.javaを追加

--- src/com/diworksdev/template/dao/UserCreateCompleteDAO.java
+++ src/com/diworksdev/template/dao/UserCreateCompleteDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import com.diworksdev.template.util.DBConnector;
 import com.diworksdev.template.util.DateUtil;
 
+
 public class UserCreateCompleteDAO {
 	private DateUtil dateUtil = new DateUtil();
 	private String sql = "INSERT INTO login_user_transaction(login_id,login_pass,user_name,insert_date) VALUES(?,?,?,?)";
@@ -33,3 +34,5 @@ public class UserCreateCompleteDAO {
 	}
 
 }
+
+
